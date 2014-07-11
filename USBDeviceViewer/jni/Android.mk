70370aea2092fdce3762444d8cfdb29729c55524
ShnaiderPavel <shnaiderpasha@gmail.com>
2014-07-11 18:16:12 +0400

    added usb camera viewer cpp, ndk build normal

--- USBDeviceViewer/jni/Android.mk
+++ USBDeviceViewer/jni/Android.mk
@@ -8,10 +8,10 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := UsbCameraViewer
 
-# LOCAL_CPPFLAGS := -DNDK_GAME_DEBUG
+# LOCAL_CPPFLAGS := -Dcxx_flags
     
-#LOCAL_SRC_FILES :=                 \
-#    main.cpp                       \
+LOCAL_SRC_FILES :=                 \
+    USBCameraViewer.cpp
 
 LOCAL_LDLIBS    := -llog -landroid#  -lEGL -lGLESv2 -lz -lOpenSLES
 #LOCAL_STATIC_LIBRARIES := #android_native_app_glue png lua
