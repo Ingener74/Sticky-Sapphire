39dce489ca39451169380b90d2fe92c4eb28133c
ShnaiderPavel <shnaiderpasha@gmail.com>
2014-07-25 10:54:38 +0400

    simplify usb camera viewer

--- USBDeviceViewer/jni/USBCameraViewer.cpp
+++ USBDeviceViewer/jni/USBCameraViewer.cpp
@@ -1,4 +1,3 @@
-
 #include <Common.h>
 #include <androidlogbuffer.h>
 #include <UsbCameraViewer.h>
@@ -8,8 +7,7 @@ using namespace std::placeholders;
 using namespace usbcv;
 
 shared_ptr<streambuf> error_buf, debug_buf;
-
-thread         th;
+thread th;
 
 struct Image
 {
@@ -19,89 +17,98 @@ struct Image
 	vector<uint8_t> buffer;
 };
 
-void drawImage(Image image)
+void drawImage( Image image )
 {
 	cout << "draw image " << image.rows << " x " << image.cols << " " << image.bpp << " " << image.buffer.size()
 			<< endl;
 }
 
-void start()
+void thread_func( promise<exception_ptr>& start, function<void( Image )> onNewImage )
 {
-    error_buf = make_shared<android_log_buffer>(cerr, ANDROID_LOG_ERROR);
-    debug_buf = make_shared<android_log_buffer>(cout);
+	uvc_context_t * ctx = nullptr;
+	uvc_device_t * dev = nullptr;
+	uvc_device_handle_t * devh = nullptr;
+	uvc_stream_ctrl_t ctrl;
 
-    try
-    {
-		exception_ptr  error;
+	try
+	{
+		uvc_error_t res;
 
-        th = thread([&error]()
-        {
-            uvc_context_t * ctx = nullptr;
-            uvc_device_t * dev = nullptr;
-            uvc_device_handle_t * devh = nullptr;
-            uvc_stream_ctrl_t ctrl;
+		res = uvc_init( &ctx, NULL );
+		if ( res < 0 ) runtime_error( "can't init uvc" );
 
-            try
-            {
-                uvc_error_t res;
+		res = uvc_find_device( ctx, &dev, 0, 0, NULL );
+		if ( res < 0 ) runtime_error( "can't find device" );
 
-                res = uvc_init(&ctx, NULL);
-                if ( res < 0 ) runtime_error("can't init uvc");
+		res = uvc_open( dev, &devh );
+		if ( res < 0 ) runtime_error( "can't open device" );
 
-                res = uvc_find_device( ctx, &dev, 0, 0, NULL);
-                if( res < 0) runtime_error("can't find device");
+		res = uvc_get_stream_ctrl_format_size( devh, &ctrl, UVC_FRAME_FORMAT_YUYV, 640, 480, 30 );
+		if ( res < 0 ) runtime_error( "can't get stream control" );
 
-                res = uvc_open(dev, &devh);
-                if (res < 0) runtime_error("can't open device");
+		res = uvc_start_streaming( devh, &ctrl, nullptr, nullptr, 0 );
+		if ( res < 0 ) runtime_error( "can't start streaming" );
 
-                res = uvc_get_stream_ctrl_format_size(devh, &ctrl, UVC_FRAME_FORMAT_YUYV, 640, 480, 30);
-                if(res < 0) runtime_error("can't get stream control");
+		start.set_value( exception_ptr() );
 
-                res = uvc_start_streaming(devh, &ctrl, nullptr, nullptr, 0);
-                if(res < 0) runtime_error("can't start streaming");
+		for ( ;; )
+		{
+//			this_thread::yield(); /* for exit */
 
-				for(;;)
-				{
-//                    uvc_stream_get_frame()
+//			uvc_stream_get_frame()
 
-					Image newImage;
-					async(bind(drawImage, newImage));
-				}
+			Image newImage;
+			onNewImage( newImage );
+		}
 
-                uvc_stop_streaming(devh);
-                uvc_close(devh);
-                uvc_unref_device(dev);
-                uvc_exit(ctx);
+		uvc_stop_streaming( devh );
+		uvc_close( devh );
+		uvc_unref_device( dev );
+		uvc_exit( ctx );
+	}
+	catch ( exception const & e )
+	{
+		start.set_exception( current_exception() );
+	}
+}
 
-            }
-            catch (exception const & e)
-            {
-                error = current_exception();
-                cerr << "UvcImageGenerator error: " << e.what() << endl;
-            }
-        });
+bool start()
+{
+	error_buf = make_shared<android_log_buffer>( cerr, ANDROID_LOG_ERROR );
+	debug_buf = make_shared<android_log_buffer>( cout );
 
-        if ( error ) rethrow_exception( error );
-    }
-    catch ( std::exception const & e )
-    {
-        cerr << "Error: " << e.what() << endl;
-    }
+	try
+	{
+		promise<exception_ptr> start_promise;
+		auto start = start_promise.get_future();
+
+		th = thread( bind( thread_func, ref( start_promise ), bind( drawImage, _1 ) ) );
+		th.detach();
+
+		start.wait();
+		if ( start.get() ) rethrow_exception( start.get() );
+
+		return true;
+	}
+	catch ( std::exception const & e )
+	{
+		cerr << "Error: " << e.what() << endl;
+		return false;
+	}
 }
 
 void stop()
 {
-    error_buf.reset();
-    debug_buf.reset();
+	error_buf.reset();
+	debug_buf.reset();
 }
 
 jboolean Java_com_shnaider_usbcameraviewer_USBCameraViewer_startUsbCameraViewer( JNIEnv * jniEnv, jobject self )
 {
-    start();
-    return true;
+	return start();
 }
 
 void Java_com_shnaider_usbcameraviewer_USBCameraViewer_stopUsbCameraViewer( JNIEnv *, jobject )
 {
-    stop();
+	stop();
 }
