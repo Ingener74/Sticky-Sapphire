38abd4d48861c9666992ffb9089561f7f81d17bd
ShnaiderPavel <shnaiderpasha@gmail.com>
2014-10-14 16:56:23 +0400

    to_string

--- USBDeviceViewer/jni/UsbCameraViewer.cpp
+++ USBDeviceViewer/jni/UsbCameraViewer.cpp
@@ -9,16 +9,16 @@ using namespace std::placeholders;
 /*
  * Definitions
  */
-template<typename Res, typename Arg>
-Res to_(Arg a)
-{
-    stringstream _;
-    _ << a;
-    Res res;
-    _ >> res;
-
-    return res;
-}
+//template<typename Res, typename Arg>
+//Res to_(Arg a)
+//{
+//    stringstream _;
+//    _ << a;
+//    Res res;
+//    _ >> res;
+//
+//    return res;
+//}
 
 class AndroidLogBuffer: public std::streambuf
 {
@@ -100,7 +100,7 @@ void thread_func(int vid, int pid, int fd, promise<exception_ptr>& start, functi
 
         if (uvc_find_device(ctx, &dev, vid, pid, NULL) < 0) throw Error("can't find device");
 
-        if (uvc_open_android(dev, &devh, fd) < 0) throw Error("can't open device " + to_<string>(fd));
+        if (uvc_open_android(dev, &devh, fd) < 0) throw Error("can't open device " + to_string(fd));
 
         if (uvc_get_stream_ctrl_format_size(devh, &ctrl, UVC_FRAME_FORMAT_YUYV, 640, 480, 30) < 0)
             throw Error("can't get stream control");
